cbeedfcd4ae6822bc1c4028a392b1dd2c3edbd7f
Christian Hueller <chuller@suse.com>
2024-10-16 16:38:47 +0200

    Add test for pulsating progress

--- usr/lib/tik/lib/cenity_tests/t_progress.sh
+++ usr/lib/tik/lib/cenity_tests/t_progress.sh
@@ -37,6 +37,32 @@ cenity c_result --progress --title="Update System Logs" \
 echo "[cenity][${c_retval}][${c_result}]"
 echo "[zenity][${z_retval}][${z_result}]"
 
+RESULT=1
+
+z_result="$((
+echo "# Updating mail logs" ; sleep 1
+echo "# Resetting cron jobs" ; sleep 1
+echo "This line will just be ignored" ; sleep 1
+echo "# Rebooting system" ; sleep 1
+) |
+zenity --progress --title="Update System Logs" \
+   --pulsate --auto-close --no-cancel)" ; z_retval=$?
+
+(
+echo "# Updating mail logs" ; sleep 1
+echo "# Resetting cron jobs" ; sleep 1
+echo "This line will just be ignored" ; sleep 1
+echo "# Rebooting system" ; sleep 1
+) |
+cenity c_result --progress --title="Update System Logs" \
+   --pulsate --auto-close --no-cancel ; c_retval=$?
+
+
+echo "[cenity][${c_retval}][${c_result}]"
+echo "[zenity][${z_retval}][${z_result}]"
+
+
+
 if [[ ${c_retval} = ${z_retval} ]] && [[ ${c_result} = ${z_result} ]]; then
   RESULT=0
 fi
