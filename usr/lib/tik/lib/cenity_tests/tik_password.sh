0a26b6da4710a216c968bd686c5eb29ab9d42db6
Christian Hueller <chuller@suse.com>
2024-10-16 21:47:37 +0200

    Add password test reproducing code from tik

--- usr/lib/tik/lib/cenity_tests/tik_password.sh
+++ usr/lib/tik/lib/cenity_tests/tik_password.sh
@@ -2,4 +2,15 @@
 
 . ./tik_functions_test
 
-
+retval=0
+#key="$(zenity --password --title='Set Encryption Passphrase')" || retval=$?
+#cenity key --password --text="This is a password text" --title="This is a PASSWORD" ; c_retval=$?
+cenity key --password --title="Set Encryption Passphrase" || retval=$?
+case $retval in
+    0)
+        echo "Key: ${key}"
+    ;;
+    1|255)
+        d --question --text="Do you really want to quit?" && exit 1
+    ;;
+esac
